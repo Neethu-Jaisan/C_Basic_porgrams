014e9a94ad97aeba88aac872b61c783c1717eaf0
Neethu Jaisan <134039864+Neethu-Jaisan@users.noreply.github.com>
2025-01-09 12:01:23 +0530

    Create Building_a_Basic_Calculator.c

---
+++ Building_a_Basic_Calculator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+int num1;
+int num2;
+printf("Enter the First number:");
+scanf("%d", &num1);
+printf("Enter the Second number:");
+scanf("%d", &num2);
+
+Printf("Sum=%d",num1+num2);
+Printf("Diff=%d",num1-num2);
+Printf("Products=%d",num1+num2);
+Printf("Division=%f",num1+num2);
+return 0;
+}
